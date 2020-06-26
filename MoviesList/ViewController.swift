4c89a728c5d45a64dc788ab36054849e89f6c709
kalonizator <halelov_adlet@mail.ru>
2020-06-26 21:37:24 +0600

    Initial Commit

---
+++ MoviesList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesList
+//
+//  Created by Адилет on 6/26/20.
+//  Copyright © 2020 kalonizator corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
